46aee0a6b637a0ae8f438a74475469a5877a09e4
sebaptcd <dev@sebastian.ghiveci.com>
2024-11-11 21:07:41 +0100

    v2.0.0.0 mk1

--- RELEASE.md
+++ RELEASE.md
@@ -1,3 +1,13 @@
+<h2><b>v2.0.0.0 mk1</b><br></h2>
+
+- rClone "bisync" has been tested and it appears to be working good. The script is transitioning towards simply handling logs, showing error notifications and processing the actual commands. Most of the drives have now been updated with "bisync".<br>
+- Fixed: Self clean script if boot sync is interrupted mid process.<br>
+- Introduced a recycle bin so that files can be double checked after deletion.<br>
+- Thanks to a functional "bisync" command and the implementation of the "recycle bin", the script can now be interrupted at any point, with confidence that data loss is not likely anymore. As a result, notifications about the script status have been removed/commented out.<br>
+- With "bisync" now applied to most of the drives, a conflict error detection has been introduced to be notified of any occurrences.<br>
+- Better handling of the timestamp function and rClone flags.<br>
+- Corrected RELEASE.md to follow the latest [iVer](https://github.com/frontfacer/iVer) format.<br>
+
 <h2><b>v1.2.1.1 mk1</b><br></h2>
 
 - Fixed typo in RELEASE.md file.<br>
@@ -24,7 +34,7 @@
 - Fixed spelling mistakes in the install instructions.<br>
 - Unified the style of the code.<br>
 
-<h2><b>v1.0.0 mk1</b><br></h2>
+<h2><b>v1.0.0.0 mk1</b><br></h2>
 
 - The script will begin by triggering the rClone sync commands in a specific order (namely: <b>orderBoot</b>).<br>
 - rClone sync commands will then run in a different order (namely: <b>orderSync</b>) after <b>orderBoot</b> completed.<br>
--- Syncing.sh
+++ Syncing.sh
@@ -6,6 +6,11 @@
 # Add to crontab at the very top: XDG_RUNTIME_DIR="/run/user/1000"
 export DISPLAY=:0
 
+# Declare the timestamp
+get_timestamp() {
+    date +%Y-%m-%d_%H%M%S
+}
+
 # Get script's directory name to use as relative path
 script_dir=$(dirname "$(realpath "$0")")
 echo "$script_dir"
@@ -19,11 +24,36 @@ boot_lock="$Setups/rClone-bysinc-workaround/Locks/Boot-Sync.lock"
 boot_ran_lock="$Setups/rClone-bysinc-workaround/Locks/Boot-Ran.lock"
 logs="$Setups/rClone-bysinc-workaround/Logs"
 
-# Declare the sync/bisync flags
-flags="--transfers 64 --checkers 48 --create-empty-src-dirs --update --progress --log-level INFO"
-bisync_flags="--force --recover --resilient"
+# Path to the rClone bin (the "recycle_bin_root" is declared in the private variables "var.sh")
+recycle_bin="$recycle_bin_root/rClone bin"
 
-timestamp=$(date +%Y-%m-%d_%H%M%S)
+# Declare the sync/bisync flags
+common_flags=(
+    --transfers 64
+    --checkers 48
+    --create-empty-src-dirs
+    --progress
+    --log-level INFO
+)
+
+sync_flags=(
+	--update
+)
+
+sync_server_local_flags=(
+	--backup-dir PLACEHOLDER_TIMESTAMP
+)
+
+sync_local_server_flags=(
+
+)
+
+bisync_flags=(
+	--fix-case
+	--force
+	--recover
+	--resilient
+)
 
 while true; do
 	# Check if the internet connection is available
@@ -32,12 +62,12 @@ while true; do
 		if [ -f "$sync_lock" ]; then
 			echo "Syncing: Past instance found, reloading in 5 minutes..."
 			notify-send "Syncing" "Past instance found, reloading in 5 minutes..."
-			mv "$sync_lock" "$logs/Sync/Canceled/Canceled_Sync_"$timestamp".log"
+			mv "$sync_lock" "$logs/Sync/Canceled/Canceled_Sync_$(get_timestamp).log"
 			sleep 300
 		elif [ -f "$boot_ran_lock" ]; then
 			echo "Syncing: Past instance found, reloading in 5 minutes..."
 			notify-send "Syncing" "Past instance found, reloading in 5 minutes..."
-			mv "$boot_ran_lock" "$logs/Boot/Canceled/Canceled_Boot_"$timestamp".log"
+			mv "$boot_ran_lock" "$logs/Boot/Canceled/Canceled_Boot_$(get_timestamp).log"
 			sleep 300
 		# Start new syncing instance.
 		elif [ ! -f "$sync_lock" ]; then
@@ -47,14 +77,28 @@ while true; do
 			sleep 10
 			# Check and prepare Boot Sync
 			# Check if Boot Syncing is not already running
-			if [ -f "$boot_lock" ]; then
+			if [ -f "$boot_lock" ] && [ $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) -ge 4 ]; then
 				# Notify that the Boot Syncing is running already and abort
-			   	echo "Syncing: Boot Sync already running, aborting."
-			   	notify-send "Syncing" "Boot Sync already running, aborting."
-			   	exit 0
+				echo "Syncing: Boot Sync already running, aborting."
+				# DEBUG: Feedback for how many instances are being detected
+				#echo $(( $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) ))
+				#notify-send $(( $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) ))
+				notify-send "Syncing" "Boot Sync already running, aborting."
+				exit 0
+			# Check if Boot Syncing was interrupted mid sync but no longer runs
+			elif [ -f "$boot_lock" ] && ! [ $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) -ge 4 ]; then
+				echo "Syncing: Active past instance found, reloading in 5 minutes..."
+				# DEBUG: Feedback for how many instances are being detected
+				#echo $(( $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) ))
+				#notify-send $(( $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) ))
+				notify-send "Syncing" "Active past instance found, reloading in 5 minutes..."
+				mv "$boot_lock" "$logs/Boot/Canceled/Canceled_Boot_$(get_timestamp).log"
+				sleep 300
 			elif [ ! -f "$boot_lock" ]; then
 				# Notify that the Boot Syncing will start
 				echo "Syncing: Boot Sync starts in 30 seconds..."
+				# DEBUG: Feedback for how many instances are being detected
+				#echo $(( $(ps aux | grep "Syncing.sh" | grep -v "grep" | wc -l) ))
 				notify-send "Syncing" "Boot Sync starts in 30 seconds..."
 				sleep 30
 				
@@ -74,7 +118,7 @@ while true; do
 						sleep 10
 
 						# Personal
-						rclone sync $flags "$Personal_remote" "$Persoanl_local" | while read line; do
+						rclone sync "${common_flags[@]}" "${sync_flags[@]}" "${sync_server_local_flags[@]}" "$Personal_remote" "$Persoanl_local" | while read line; do
 							# Check for errors
 							if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 								# Send a notification
@@ -88,9 +132,9 @@ while true; do
 							if ping -c 1 8.8.8.8 &> /dev/null; then
 						
 								# Personal/Public
-								rclone bisync $bisync_flags $flags "$PersonalPb_remote" "$PersoanlPb_local" | while read line; do
+								rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$PersonalPb_remote" "$PersoanlPb_local" | while read line; do
 									# Check for errors
-									if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+									if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 										# Send a notification
 										notify-send "Syncing Error:" "$line"
 									fi
@@ -102,9 +146,9 @@ while true; do
 									if ping -c 1 8.8.8.8 &> /dev/null; then
 								
 										# Dev
-										rclone sync $flags "$Dev_remote" "$Dev_local" | while read line; do
+										rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$Dev_remote" "$Dev_local" | while read line; do
 											# Check for errors
-											if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+											if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 												# Send a notification
 												notify-send "Syncing Error:" "$line"
 											fi
@@ -116,9 +160,9 @@ while true; do
 											if ping -c 1 8.8.8.8 &> /dev/null; then
 										
 												# WORKSTATION
-												rclone sync $flags "$WORKSTATION_remote" "$WORKSTATION_local" | while read line; do
+												rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$WORKSTATION_remote" "$WORKSTATION_local" | while read line; do
 													# Check for errors
-													if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+													if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 														# Send a notification
 														notify-send "Syncing Error:" "$line"
 													fi
@@ -126,7 +170,7 @@ while true; do
 												done
 										
 												# Mark that the Boot Syncing is complete
-												mv "$boot_lock" "$logs/Boot/Boot_"$timestamp".log"
+												mv "$boot_lock" "$logs/Boot/Boot_$(get_timestamp).log"
 												touch "$boot_ran_lock"
 												# Notify that the Boot Syncing is complete
 												echo "Syncing: Boot Sync completed."
@@ -149,6 +193,13 @@ while true; do
 													rm "$boot_ran_lock"
 
 													while true; do
+
+													# ==============================
+													# Cycle begins now, in this loop
+													# ==============================
+
+													dynamic_sync_server_local_flags=("${sync_server_local_flags[@]//PLACEHOLDER_TIMESTAMP/$recycle_bin/$(get_timestamp)}")
+
 														# Check if another instance has started in parallel
 														if [ -f "$boot_lock" ] || [ -f "$boot_ran_lock" ]; then
 															# Notify that another instance started
@@ -163,13 +214,11 @@ while true; do
 																# rclone sync | local -> server
 																#
 
-																# Creating a timestamp for Sync
-																timestampSync=$(date +%Y-%m-%d_%H%M%S)
 																# Mark that the syncing cycle will start
 																touch "$sync_lock"
 																# Notify that the syncing cycle will start
 																echo "Syncing: Starting in 30 seconds..."
-																notify-send "Syncing" "Starting in 30 seconds..."
+																#notify-send "Syncing" "Starting in 30 seconds..."
 																# Give a 30 seconds response time
 																sleep 30
 																
@@ -179,11 +228,11 @@ while true; do
 
 																		# Notify that syncing local -> server will start
 																		echo "Syncing: Uploading on the server..."
-																		notify-send "Syncing" "Uploading on the server..."
+																		#notify-send "Syncing" "Uploading on the server..."
 																		sleep 10
 																
 																		# Personal
-																		rclone sync $flags "$Persoanl_local" "$Personal_remote" | while read line; do
+																		rclone sync "${common_flags[@]}" "${sync_flags[@]}" "${sync_local_server_flags[@]}" "$Persoanl_local" "$Personal_remote" | while read line; do
 																			# Check for errors
 																			if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 																				# Send a notification
@@ -197,9 +246,9 @@ while true; do
 																			#if ping -c 1 8.8.8.8 &> /dev/null; then
 																		
 																				# Personal/Public
-																				#rclone bisync $bisync_flags $flags "$PersoanlPb_local" "$PersonalPb_remote" | while read line; do
+																				#rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$PersoanlPb_local" "$PersonalPb_remote" | while read line; do
 																					# Check for errors
-																				#	if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																				#	if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 																				#		# Send a notification
 																				#		notify-send "Syncing Error:" "$line"
 																				#	fi
@@ -208,35 +257,35 @@ while true; do
 																		
 																				while true; do
 																					# Check if the internet connection is available
-																					if ping -c 1 8.8.8.8 &> /dev/null; then
+																					#if ping -c 1 8.8.8.8 &> /dev/null; then
 																				
 																						# Dev
-																						rclone sync $flags "$Dev_local" "$Dev_remote" | while read line; do
-																							# Check for errors
-																							if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
-																								# Send a notification
-																								notify-send "Syncing Error:" "$line"
-																							fi
-																							echo "$line" >> "$sync_lock"
-																						done
+																						#rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$Dev_local" "$Dev_remote" | while read line; do
+																						#	# Check for errors
+																						#	if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																						#		# Send a notification
+																						#		notify-send "Syncing Error:" "$line"
+																						#	fi
+																						#	echo "$line" >> "$sync_lock"
+																						#done
 
 																						while true; do
 																							# Check if the internet connection is available
-																							if ping -c 1 8.8.8.8 &> /dev/null; then
+																							#if ping -c 1 8.8.8.8 &> /dev/null; then
 																			
 																								# WORKSTATION
-																								rclone sync $flags "$WORKSTATION_local" "$WORKSTATION_remote" | while read line; do
-																									# Check for errors
-																									if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
-																										# Send a notification
-																										notify-send "Syncing Error:" "$line"
-																									fi
-																									echo "$line" >> "$sync_lock"
-																								done
+																								#rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$WORKSTATION_local" "$WORKSTATION_remote" | while read line; do
+																								#	# Check for errors
+																								#	if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																								#		# Send a notification
+																								#		notify-send "Syncing Error:" "$line"
+																								#	fi
+																								#	echo "$line" >> "$sync_lock"
+																								#done
 																								
 																								# Notify that syncing local -> server was completed
 																								echo "Syncing: Server upload - done."
-																								notify-send "Syncing" "Server upload - done."
+																								#notify-send "Syncing" "Server upload - done."
 																								sleep 10
 																								
 																								#
@@ -249,11 +298,11 @@ while true; do
 
 																										# Notify that syncing server -> local will start
 																										echo "Syncing: Downloading from the server..."
-																										notify-send "Syncing" "Downloading from the server..."
+																										#notify-send "Syncing" "Downloading from the server..."
 																										sleep 10
 
 																										# Personal
-																										rclone sync $flags "$Personal_remote" "$Persoanl_local" | while read line; do
+																										rclone sync "${common_flags[@]}" "${sync_flags[@]}" "${dynamic_sync_server_local_flags[@]}" "$Personal_remote" "$Persoanl_local" | while read line; do
 																											# Check for errors
 																											if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 																												# Send a notification
@@ -262,14 +311,24 @@ while true; do
 																											echo "$line" >> "$sync_lock"
 																										done
 																										
+																										# Personal (local bisync)
+																										rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$Persoanl_local_bisync" "$Personal_local_mainstream" | while read line; do
+																											# Check for errors
+																											if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																												# Send a notification
+																												notify-send "Syncing Error:" "$line"
+																											fi
+																											echo "$line" >> "$sync_lock"
+																										done
+
 																										while true; do
 																											# Check if the internet connection is available
 																											if ping -c 1 8.8.8.8 &> /dev/null; then
 																										
 																												# Personal/Public
-																												rclone bisync $bisync_flags $flags "$PersonalPb_remote" "$PersoanlPb_local" | while read line; do
+																												rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$PersonalPb_remote" "$PersoanlPb_local" | while read line; do
 																													# Check for errors
-																													if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																													if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 																														# Send a notification
 																														notify-send "Syncing Error:" "$line"
 																													fi
@@ -281,9 +340,9 @@ while true; do
 																													if ping -c 1 8.8.8.8 &> /dev/null; then
 																												
 																														# Dev
-																														rclone sync $flags "$Dev_remote" "$Dev_local" | while read line; do
+																														rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$Dev_remote" "$Dev_local" | while read line; do
 																															# Check for errors
-																															if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																															if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 																																# Send a notification
 																																notify-send "Syncing Error:" "$line"
 																															fi
@@ -295,9 +354,9 @@ while true; do
 																															if ping -c 1 8.8.8.8 &> /dev/null; then
 																														
 																																# WORKSTATION
-																																rclone sync $flags "$WORKSTATION_remote" "$WORKSTATION_local" | while read line; do
+																																rclone bisync "${common_flags[@]}" "${bisync_flags[@]}" "$WORKSTATION_remote" "$WORKSTATION_local" | while read line; do
 																																	# Check for errors
-																																	if [[ "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
+																																	if [[ "$line" == *".conflict"* || "$line" == *"ERROR"* || "$line" == *"Errors:"* ]]; then
 																																		# Send a notification
 																																		notify-send "Syncing Error:" "$line"
 																																	fi
@@ -306,7 +365,7 @@ while true; do
 																																
 																																# Notify that syncing server -> local was completed
 																																echo "Syncing: Server download - done."
-																																notify-send "Syncing" "Server download - done."
+																																#notify-send "Syncing" "Server download - done."
 																																sleep 10
 																																
 																																break
@@ -346,21 +405,21 @@ while true; do
 																								done
 
 																								break
-																							else
-																								# No internet connection
-																								echo "No internet. Reloading in 1 minute."
-																								notify-send "Syncing" "(Cycle - UP WORKSTATION) No internet. Retrying in 1 minute."
-																								sleep 60
-																							fi
+																							#else
+																							#	# No internet connection
+																							#	echo "No internet. Reloading in 1 minute."
+																							#	notify-send "Syncing" "(Cycle - UP WORKSTATION) No internet. Retrying in 1 minute."
+																							#	sleep 60
+																							#fi
 																						done
 
 																						break
-																					else
-																						# No internet connection
-																						echo "No internet. Reloading in 1 minute."
-																						notify-send "Syncing" "(Cycle - UP Dev) No internet. Retrying in 1 minute."
-																						sleep 60
-																					fi
+																					#else
+																					#	# No internet connection
+																					#	echo "No internet. Reloading in 1 minute."
+																					#	notify-send "Syncing" "(Cycle - UP Dev) No internet. Retrying in 1 minute."
+																					#	sleep 60
+																					#fi
 																				done
 																				
 																				break
@@ -382,10 +441,10 @@ while true; do
 																done
 
 																# Mark that the syncing cycle was completed
-																mv "$sync_lock" "$logs/Sync/Sync_"$timestampSync".log"
+																mv "$sync_lock" "$logs/Sync/Sync_$(get_timestamp).log"
 																# Notify that the syncing cycle was completed
 																echo "Syncing: Complete."
-																notify-send "Syncing" "Complete."
+																#notify-send "Syncing" "Complete."
 																# Give a 10 minutes cool-down before the next cycle
 																sleep 600
 
